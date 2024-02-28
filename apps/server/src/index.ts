2107b9f69160a11380f12ba01de4af00b303b7ef
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-02-28 17:59:18 -0500

    Added basic socket code

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, world!");
+import http from "http";
+import SocketService from "./services/socket";
+
+async function init() {
+  const httpServer = http.createServer();
+  const socketService = new SocketService();
+  const PORT = process.env.PORT || 8000;
+
+  socketService.io.attach(httpServer);
+
+  socketService.initListeners();
+
+  httpServer.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+}
+init();
---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,29 @@
+import { Server } from "socket.io";
+
+class SocketService {
+  private _io: Server;
+  constructor() {
+    console.log("SocketService initialized");
+    this._io = new Server();
+  }
+
+  get io(): Server {
+    return this._io;
+  }
+
+  /**
+   * initListeners
+   */
+  public initListeners() {
+    const io = this._io;
+    io.on("connection", (socket) => {
+      console.log("A user connected ", socket.id);
+
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("message", message);
+        // io.emit("event:message", message);
+      });
+    });
+  }
+}
+export default SocketService;
